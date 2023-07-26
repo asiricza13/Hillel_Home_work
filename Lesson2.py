098bf85656249004423a9a1f993a667cfaaa5e44
asiricza13 <140596459+asiricza13@users.noreply.github.com>
2023-07-26 20:34:40 +0300

    Initial commit

---
+++ Lesson2.py
@@ -0,0 +1,29 @@
+from math import sqrt
+
+# import this
+# import antigravity
+# import __hello__
+# from __future__import braces
+
+a = 1
+b = 5
+c = 4
+x1 = (-b+sqrt(b**2-4*a*c))/2*a
+print(x1)
+x2 = (-b-sqrt(b**2-4*a*c))/2*a
+print(x2)
+
+print("Як твоє ім'я?:")
+yourName = input()
+print("Привіт, ", yourName)
+
+
+a = int(input("Введіть кількість гривень:"))
+b = 37.7
+c = a/b
+print("Станом на 15 липня 2022 року це становить",c , "Долари США")
+
+
+
+
+
---
+++ Lesson3.py
@@ -0,0 +1,28 @@
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y == 0:
+        print("Ділення на нуль неможливе.")
+        return
+    return x / y
+
+arg1 = float(input("Веддіть перше число:"))
+arg2 = float(input("Введіть друге число:"))
+operation = input("Введіть математичну операцію (+, -, *, /):")
+
+if operation == "+":
+    result = add(arg1, arg2)
+elif operation == "-":
+    result = subtract(arg1, arg2)
+elif operation == "*":
+    result = multiply(arg1, arg2)
+elif operation == "/":
+    result = divide(arg1, arg2)
+else:
+    result = "Недопустима операція"
+
+print("Результат:", result)
